8df0e53dd313d947ffcba300ec25c6f3199383da
User <user@example.com>
2026-07-07 23:25:44 +0800

    authentication fix

--- convex/auth.ts
+++ convex/auth.ts
@@ -2,7 +2,7 @@ import { convexAuth } from "@convex-dev/auth/server";
 import { Password } from "@convex-dev/auth/providers/Password";
 import { Email } from "@convex-dev/auth/providers/Email";
 
-export const { auth, signIn, signOut, store } = convexAuth({
+export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
   providers: [
     Password({
       id: "password",
